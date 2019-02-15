3b137c92360c3a3de1e5b8480eba7bc6c829fc42
Ben Wagner <benjamin.j.wagner.1@gmail.com>
2019-02-15 14:26:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Alfred Cardhop
+Run Cardhop queries through Alfred
+
+## Description
+A simple [Alfred](http://www.alfredapp.com/) workflow to search, add, update, call, message, or copy contacts by passing queries through to the [Cardhop](http://flexibits.com/cardhop) desktop application.
+
+
+### Screenshots
+![Screenshot 1](screenshot1.png)
+![Screenshot 2](screenshot2.png)
+
+
+#### Contributors
+Workflow created by [blizzrdof77](https://blizzrdof77.com) of [Simple Digital](https://simpledigital.net).
---
+++ index.js
@@ -0,0 +1,35 @@
+const alfy = require('alfy')
+const helpers = require('./lib/applescript-helpers.js')
+
+const input = (alfy.input || 'brian this is a message').split(' ')
+const query = input[0]
+const message = input.slice(1, input.length).join(' ').trim();
+
+(async () => {
+  let output = await helpers.buddySearch(query)
+  if (!output) {
+    return alfy.output([{
+      title: `No results found for ${query}`,
+      subtitle: 'Please try another query'
+    }])
+  }
+
+  alfy.output(output.map(buddy => {
+    let displayMessage = message
+    if (message.length > 60) {
+      const first = message.slice(0, 20)
+      const last = message.slice(message.length - 20, message.length)
+      displayMessage = `${first}.............${last}`
+    }
+
+    return {
+      title: buddy.name,
+      subtitle: message ? `Send "${displayMessage}" to ${buddy.handle}` : `Type to send message to ${buddy.handle}`,
+      arg: JSON.stringify({
+        handle: buddy.handle,
+        name: buddy.name,
+        message
+      })
+    }
+  }))
+})()
